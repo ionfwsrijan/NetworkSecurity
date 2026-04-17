6dc0857e314ca9f8dfb98904cfa6e13bc6ac5f36
Srijan Jaiswal <jaiswalsrijan505@gmail.com>
2026-04-17 23:57:27 +0530

    refactor: Remove print statements for cleaner code in config_entity.py

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -2,9 +2,6 @@ from datetime import datetime
 import os
 from networksecurity.constant import training_pipeline
 
-print (training_pipeline.PIPELINE_NAME)
-print (training_pipeline.ARTIFACT_DIR)
-
 class TrainingPipelineConfig:
     def __init__(self,timestamp:str = datetime.now()):
         timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
